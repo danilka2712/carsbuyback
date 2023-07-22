11d1d294525b1ba2cc755c9eb23cc8faa91c5a09
unknown <danilka2712@yandex.ru>
2023-07-22 19:20:35 +0600

    first commit

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,6 +2,9 @@
     import { page } from "$app/stores";
 </script>
 
-<div class=" bg-white sm:px-16 p-4 h-screen">
-    <h1 class="text-5xl">{$page.status}: {$page.error?.message}</h1>
+<div
+    class=" bg-white sm:px-16 p-4 h-screen flex flex-col items-center justify-center"
+>
+    <h1 class="text-6xl">{$page.status}: {$page.error?.message}</h1>
+    <a href="/">Назад</a>
 </div>
