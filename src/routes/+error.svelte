d25a32448b3e4980f51d048f5d51da61fc88083e
unknown <danilka2712@yandex.ru>
2023-07-22 19:14:48 +0600

    first commit

---
+++ src/routes/+error.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import { page } from "$app/stores";
+</script>
+
+<div class=" bg-white sm:px-16 p-4 h-screen">
+    <h1 class="text-5xl">{$page.status}: {$page.error?.message}</h1>
+</div>
