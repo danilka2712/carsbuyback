ebd1b62a7b71f313f13510d18103c9fd4be725f2
unknown <danilka2712@yandex.ru>
2023-07-22 19:21:49 +0600

    first commit

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div
-    class=" bg-white sm:px-16 p-4 h-screen flex flex-col items-center justify-center"
+    class=" bg-white sm:px-16 p-4 h-[70vh] flex flex-col items-center justify-center"
 >
     <h1 class="text-6xl">{$page.status}: {$page.error?.message}</h1>
     <a href="/">Назад</a>
