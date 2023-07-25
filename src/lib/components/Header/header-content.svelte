861a3a563ecc87d4fdaa6e24b03d9c46e98289b7
unknown <danilka2712@yandex.ru>
2023-07-26 00:01:16 +0600

    first commit

--- src/lib/components/Header/header-content.svelte
+++ src/lib/components/Header/header-content.svelte
@@ -15,4 +15,9 @@
     <a class="sm:flex hidden font-medium" href="tel:+79088002187"
         >+7 (908) 800 21 87 | 24/7</a
     >
+    <a
+        class="h-7 w-7 rounded-full bg-black flex items-center justify-center"
+        href="tel:+79088002187"
+        ><img class="w-[50%]" src="/svg/phone-call-01.svg " alt="" /></a
+    >
 </div>
