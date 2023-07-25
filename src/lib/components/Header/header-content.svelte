a70b815b75043070e4e958eb04e3ca28d4368640
unknown <danilka2712@yandex.ru>
2023-07-25 23:34:55 +0600

    first commit

--- src/lib/components/Header/header-content.svelte
+++ src/lib/components/Header/header-content.svelte
@@ -12,5 +12,5 @@
     class="flex justify-between items-center border-b border-gray-100 w-full pt-10 pb-6"
 >
     <a class=" shrink-0" href="/"><slot name="logotype" /> </a>
-    <a class="sm:flex hidden font-medium" href="">+7 (908) 119-72-79 | 24/7</a>
+    <a class="sm:flex hidden font-medium" href="tel:+79088002187>+7 (908) 800 21 87 | 24/7</a>
 </div>
